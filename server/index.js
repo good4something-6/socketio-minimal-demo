cd67a3d60fb63b0e1128ef8ed0db88ee55fa433b
good4something-6 <mail6mw@gmail.com>
2022-04-26 10:56:40 +0100

    Minor changes

--- server/index.js
+++ server/index.js
@@ -1,28 +1,27 @@
+const http = require("http");
+const server = http.createServer();
 
-const http = require('http').createServer();
-
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+const io = require("socket.io")(server, {
+  cors: { origin: "*" },
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
-    });
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id} said ${message}`);
+  });
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
-
+server.listen(8080, () => console.log("listening on http://localhost:8080"));
 
 // Regular Websockets
 
 // const WebSocket = require('ws')
 // const server = new WebSocket.Server({ port: '8080' })
 
-// server.on('connection', socket => { 
+// server.on('connection', socket => {
 
 //   socket.on('message', message => {
 
@@ -31,6 +30,3 @@ http.listen(8080, () => console.log('listening on http://localhost:8080') );
 //   });
 
 // });
-
-
- 
